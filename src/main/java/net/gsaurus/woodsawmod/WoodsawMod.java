2febf85ceed2eb92565e42709d4b1efe8731f1b6
Gunnar <160498180+GunnarWaem@users.noreply.github.com>
2025-11-30 17:26:41 +0100

    Initial commit

---
+++ src/main/java/net/gsaurus/woodsawmod/WoodsawMod.java
@@ -0,0 +1,16 @@
+package net.gsaurus.woodsawmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WoodsawMod implements ModInitializer {
+	public static final String MOD_ID = "woodsawmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/gsaurus/woodsawmod/WoodsawModClient.java
@@ -0,0 +1,10 @@
+package net.gsaurus.woodsawmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WoodsawModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
